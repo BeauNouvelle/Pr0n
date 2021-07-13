fa168dedec71194a007fd6905b90a19e5b2f40f8
Beau Nouvelle <beau@getswifty.dev>
2021-07-13 21:37:13 +1000

    Initial Commit

---
+++ Shared/Pr0nApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pr0nApp.swift
+//  Shared
+//
+//  Created by Beau Nouvelle on 13/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Pr0nApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
